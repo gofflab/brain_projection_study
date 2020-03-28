8b00dedd005352c7de088ee1bd17be7e5c7dfd0b
wli90 <55749822+wli90@users.noreply.github.com>
2020-03-28 19:24:54 -0400

    Add files via upload

---
+++ Code/2Dor3D.R
@@ -0,0 +1,35 @@
+Distinguish2Dor3D<-function(pattern, row = TRUE, z, k = 3.5 ){
+  
+ if(row == FALSE){
+   pattern <- t(pattern)
+ }
+
+## calculated mean for each slice
+  m <- matrix(, nrow = dim(pattern)[1], ncol = 0)
+  
+  i<-1
+  for(g in unique(z)){
+    m<-cbind(m, apply(pattern[,which(z == g)], 1, mean))
+    i = i+1
+  }
+
+  
+ result<-c()
+ 
+ for(p in 1:dim(m)[1]){
+   #get the 0.25 and 0.75 qauntile  
+   x <- quantile(m[p,])[c(2,4)]
+  
+   #calculate the critical value for numeric outlier
+   # k = 2.5-9.5 is a good value 
+   c <- (x[2]-x[1])*k
+  
+   #find outlier, if outlier is found, the pattern is 2D, assgin 2 to result. if not found, pattern is 3D, assign 3 to result.
+   if ( max(m[p,]) > c){
+     result<- c(result,2)
+   }else{
+    result<-c(result,3)
+   }
+ }
+ return(which(result == 3))
+}
